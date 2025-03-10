98b07bd1f6ebe1d385777e080454599a7d472040
RoisinMitchell <21193762@studentmail.ul.ie>
2025-03-10 17:46:05 +0000

    Updated Directory Structure & Added Left Navigation Bar
    - Restructured directories
    - Implemented responsive left-navbar

---
+++ the_artist_harbour/config/config.php
@@ -0,0 +1,1 @@
+<!-- Site wide configuration like DB credentials... -->
--- the_artist_harbour/public/index.php
+++ the_artist_harbour/public/index.php
@@ -5,43 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MyApp</title>
-
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-
-    <style>
-        /* Makes sure the content takes up full page of space. Without this footer is directly under content not forced to bottom */
-        html,
-        body {
-            height: 100%;
-            margin: 0;
-            display: flex;
-            flex-direction: column;
-        }
-
-        /* Pushes footer to the bottom of the page all page content must be labelled with this class */
-        .content {
-            flex: 1;
-        }
-
-        .footer {
-            background-color: #82689A;
-            padding: 15px;
-            color: white;
-            text-align: center;
-        }
-    </style>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-    <?php include __DIR__ . '/../includes/header.php'; ?>
+    <?php include __DIR__ . '/../templates/navbar-top.php'; ?>
 
-    <div
-        class="content container d-flex flex-column justify-content-center align-items-center text-center amarante-regular">
-        <h1>Welcome to The Artist Harbour!</h1>
+    <div>
+        <div class="row">
+            <div class="col navbar-left">
+                <?php include __DIR__ . '/../templates/navbar-left.php'; ?>
+            </div>
+        </div>
     </div>
-
-    <?php include __DIR__ . '/../includes/footer.php'; ?>
 
 </body>
 
