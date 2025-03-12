74b389822ec249fa6392e65db0e5d237bd2ea0e1
Katie Purser <22345477@studentmail.ul.ie>
2025-03-12 23:45:01 +0000

    Planning of Home Page

--- the_artist_harbour/public/home_page.php
+++ the_artist_harbour/public/home_page.php
@@ -6,6 +6,24 @@
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css bootstrap.min.css"
             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
             crossorigin="anonymous">
+        <link rel=" stylesheet" href="assets/css/styles.css">
         
     </head>
+    <body>
+        <div class="row">
+            <div class="col-12">
+                <title>WELCOME TO THE ARTIST HARBOUR</title>
+            </div>
+        </div>
+
+<!-- PHP script to retrieve top rated services from database -->
+<!-- SELECT * FROM services ORDER BY rating -->
+<!-- if there are at least 10 entries in the table, iterate through the top 10 and lay them out -->
+<!-- if there are less than 10, just display however many there are -->
+
+        <div class="row">
+            <
+        </div>
+        
+    </body>
 </html>
