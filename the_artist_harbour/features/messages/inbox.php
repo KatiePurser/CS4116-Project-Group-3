6aa201aaf62a17871abd6c5a1eff450cbd1450d3
RoisinMitchell <21193762@studentmail.ul.ie>
2025-03-13 21:23:01 +0000

    Fixed pathing
    Fixed pathing in inbox.php

--- the_artist_harbour/features/messages/inbox.php
+++ the_artist_harbour/features/messages/inbox.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MyApp</title>
 
-    <link rel="stylesheet" href="/public/css/styles.css">
+    <link rel="stylesheet" href="../../public/css/styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
