8ff47b46e9f08b6955a81c212c814c40d6bdf01d
Katie Purser <22345477@studentmail.ul.ie>
2025-03-21 21:43:44 +0000

    Added correct processing for SQL queries

--- the_artist_harbour/utilities/serviceDetails.php
+++ the_artist_harbour/utilities/serviceDetails.php
@@ -8,22 +8,55 @@
 
 class ServiceDetails{
 
+    private static $servername = "the-artist-harbour.cl64o2auodev.eu-north-1.rds.amazonaws.com";
+    private static $username = "admin";
+    private static $password = "password2025";
+    private static $dbname = "artist_harbour_db";
+
     public static function getServicePrice($serviceId){
         include_once("../utilities/DatabaseHandler.php");
-        $sql = "SELECT min_price, max_price FROM services WHERE service_id={$serviceId}";
-        $result = DatabaseHandler::make_select_query($sql);
-        $prices = mysqli_fetch_assoc($result);
-        if($service["min_price"]!=NULL){
-            return "€{$max_price}";
+        // $sql = "SELECT min_price, max_price FROM services WHERE id=$serviceId";
+
+        $conn = new mysqli(static::$servername, static::$username, static::$password, static::$dbname);
+        
+        if ($conn->connect_error) {
+            error_log("Connection failed: " . $conn->connect_error);
+            return null;
+        }
+
+        $stmt = $conn->prepare("SELECT * FROM services WHERE id=?");
+        $stmt->bind_param("s", $serviceId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $prices = $result->fetch_assoc();
+
+        if(!isset($prices["min_price"]) && !isset($prices["max_price"])){
+            echo"UH OH";
+        }else if(!isset($prices["min_price"])){
+            return "€{$prices["max_price"]}";
         }else{
-            return "€{$min_price} - €{$max_price}";
+            return "€{$prices["min_price"]} - €{$prices["max_price"]}";
         }
     }
 
     public static function getServiceRating($serviceId){
-        include_once("../utilities/DatabaseHandler.php");
-        $sql = "SELECT AVG(rating) FROM reviews WHERE service_id={$serviceId}";
-        $rating = DatabaseHandler::make_select_query($sql);
-        return $rating;
+        $conn = new mysqli(static::$servername, static::$username, static::$password, static::$dbname);
+        
+        if ($conn->connect_error) {
+            error_log("Connection failed: " . $conn->connect_error);
+            return null;
+        }
+
+        $stmt = $conn->prepare("SELECT rating FROM reviews WHERE service_id=?");
+        $stmt->bind_param("s", $serviceId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $rating = $result->fetch_assoc();
+
+        if(!isset($rating["rating"])){
+            return "Not yet reviewed";
+        }else{
+            return "{$rating["rating"]}/5";
+        }
     }
 }
