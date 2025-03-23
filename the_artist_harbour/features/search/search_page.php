baf65249f253b8cc7918c0e1a2b829cb227eb8ce
Katie Purser <22345477@studentmail.ul.ie>
2025-03-23 22:53:36 +0000

    Created search results page

---
+++ the_artist_harbour/features/search/search_page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Search Results</title>
+        <link rel="stylesheet" href="public/css/styles.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body style="padding-top: 73.6px;">
+        <?php 
+            $keyword=$_GET["search"];
+            
+        ?>
+    </body>
+</html>
