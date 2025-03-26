7251b807051f585cde31de8dd81c7089bfe79e04
RoisinMitchell <21193762@studentmail.ul.ie>
2025-03-26 22:56:47 +0000

    Added session info in messaging system
    Added session info in messaging system

--- the_artist_harbour/features/messages/inbox.php
+++ the_artist_harbour/features/messages/inbox.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <?php require 'scripts/load_current_conversation.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,16 +11,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MyApp</title>
     <link rel="stylesheet" href="../../public/css/styles.css">
+    <link rel="stylesheet" href="../../public/scss/style.scss">
     <link rel="stylesheet" href="css/messaging-styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        div {
+            padding: 0 !important;
+        }
+    </style>
 </head>
 
-
-
 <body>
-    <div>
+    <div class="container-fluid">
         <div class="row g-0">
             <div class="col-12">
                 <?php include __DIR__ . '/../../templates/header.php'; ?>
--- the_artist_harbour/features/messages/scripts/send_message.php
+++ the_artist_harbour/features/messages/scripts/send_message.php
@@ -1,14 +1,15 @@
 <?php
+session_start();
 require_once __DIR__ . '/../../../utilities/databaseHandler.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $sender_id = 2; // User ID
+    $sender_id = $_SESSION['user_id'];
     $receiver_id = $_POST["receiver_id"];
     $message_text = trim($_POST["message_text"]);
     $status = 'accepted';
 
     $sql = "INSERT INTO messages (sender_id, receiver_id, text, status, created_at)
-            VALUES ($sender_id, $receiver_id, '$message_text', $status, NOW())";
+            VALUES ($sender_id, $receiver_id, '$message_text', '$status', NOW())";
 
     $result = DatabaseHandler::make_modify_query($sql);
 
