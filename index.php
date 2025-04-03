306e7f0753d33feea3f4a62faaa3365cafda4923
RoisinMitchell <21193762@studentmail.ul.ie>
2025-04-03 21:05:47 +0100

    Fix or add feature

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: CS4116-Project-Group-3/the_artist_harbour/features/registration-login/login.php");
+header("Location: the_artist_harbour/features/registration-login/login.php");
 exit();
 ?>
