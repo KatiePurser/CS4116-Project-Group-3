c3b5fadeaf412d6454ce8045d06fee2aee7b6365
Sohaila Awaga <131364738+sohailaawaga@users.noreply.github.com>
2025-04-04 13:03:34 +0100

    Revert "Fix or add feature"
    This reverts commit 629f00c7c8370f1ca239d71a88b32d5ddfdfbe0f.

--- the_artist_harbour/features/search/search_page.php
+++ the_artist_harbour/features/search/search_page.php
@@ -18,7 +18,7 @@ if ($_SESSION['user_type'] != 'customer') {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Search Results</title>
-        <link rel="stylesheet" href="../../public/css/styles.css">
+        <link rel="stylesheet" href="public/css/styles.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
