1634547dcfc08fab3a57d1a98d5baae398de89e7
Katie Purser <22345477@studentmail.ul.ie>
2025-04-15 22:47:29 +0100

    Add functions for tags, business names and images

--- the_artist_harbour/features/search/searchMethods.php
+++ the_artist_harbour/features/search/searchMethods.php
@@ -9,6 +9,48 @@
             fclose($file_handle);
             return $line_of_text;
         }
+
+        public static function formatTags($tags){
+            $tags_array = explode("," , $tags);
+            $i=0;
+            $tags_return = "";
+            while($i<count($tags_array)){
+                $tags_return.=$tags_array[$i];
+                if($i<count($tags_array)-1){
+                    $tags_return.=", ";
+                }
+                $i++;
+            }
+            return $tags_return;
+        }
+
+        public static function getBusinessName($businesses, $business_id){
+            $i=0;
+            $cond=false;
+            while($cond==false && $i<count($businesses)){
+                if($businesses[$i]['id']==$business_id){
+                    $business_name = $businesses[$i]['display_name'];
+                    $cond=true;
+                }
+                $i++;
+            }
+            return $business_name;
+        }
+        
+        public static function getProfileImage($users, $user_id){
+            $i=0;
+            while($i<count($users)){
+                if($users[$i]['id']==$user_id){
+                    if(isset($users[$i]['profile_picture'])){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+                $i++;
+            }
+
+        }
     }
 
 ?>
