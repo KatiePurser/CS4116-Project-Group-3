58e5627b1cee6d673bbb5697b23452ded56814e3
Katie Purser <22345477@studentmail.ul.ie>
2025-04-21 22:03:52 +0100

    JS for processing and displaying modal operation outcomes
    Currently only displays outcome text for insight requests - will possibly need to be reworked

---
+++ the_artist_harbour/features/service/js/outcome_handling.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const actionStatus = urlParams.get('outcome');
+    const action = urlParams.get('action')
+    
+    if(action == '0'){
+        const reviewReportOutcomeModal = new bootstrap.Modal(document.getElementById('reviewReportOutcomeModal'));
+        if (actionStatus) {
+            const actionAlert = document.getElementById('actionFeedbackReview');
+            actionAlert.classList.remove('d-none');
+            actionAlert.classList.add(actionStatus === 'success' ? 'alert-success' : 'alert-danger');
+            actionAlert.textContent = actionStatus === 'success'
+                ? 'Report submitted successfully!'
+                : 'Failed to submit report. Please try again.';
+            reviewReportOutcomeModal.show();
+        }
+    }else if(action == '1'){
+        const insightRequestOutcomeModal = new bootstrap.Modal(document.getElementById('insightRequestOutcomeModal'));
+        if (actionStatus) {
+            const actionAlert = document.getElementById('actionFeedback');
+            actionAlert.classList.remove('d-none');
+            actionAlert.classList.add(actionStatus === 'success' ? 'alert-success' : 'alert-danger');
+            actionAlert.textContent = actionStatus === 'success'
+                ? 'Insight request sent successfully!'
+                : 'Failed to send insight request. Please try again.';
+            insightRequestOutcomeModal.show();
+        }
+    }else if(action == '2'){
+        const serviceRequestOutcomeModal = new bootstrap.Modal(document.getElementById('serviceRequestOutcomeModal'));
+        if(actionStatus) {
+            const actionAlert = document.getElementById('actionFeedback');
+            actionAlert.classList.remove('d-none');
+            actionAlert.classList.add(actionStatus === 'success' ? 'alert-success' : 'alert-danger');
+            actionAlert.textContent = actionStatus === 'success'
+                ? 'Service request sent successfully!'
+                : 'Failed to send service request. Please try again.';
+            serviceRequestOutcomeModal.show();
+        }
+    }
+});
